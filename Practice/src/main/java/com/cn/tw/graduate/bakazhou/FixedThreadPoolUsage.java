e2de8794cbe7c80a376497ce81bc1d5ea1829d92
bakazhou <706515174@qq.com>
2022-09-26 13:41:42 +0800

    feat: completed FixedThreadPoolUsage example

--- Practice/src/main/java/com/cn/tw/graduate/bakazhou/FixedThreadPoolUsage.java
+++ Practice/src/main/java/com/cn/tw/graduate/bakazhou/FixedThreadPoolUsage.java
@@ -1,4 +1,33 @@
 package com.cn.tw.graduate.bakazhou;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class FixedThreadPoolUsage {
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService fixedThreadPool = Executors.newFixedThreadPool(3);
+        Object lock = new Object();
+        Set<Long> threadSet = new HashSet<>();
+        for (int i = 0; i < 6; i++) {
+            fixedThreadPool.execute(() -> {
+                synchronized (lock){
+                    System.out.println("Current thread id:"+Thread.currentThread().getId());
+                    threadSet.add(Thread.currentThread().getId());
+                    System.out.println("Current thread name:"+Thread.currentThread().getName());
+                    System.out.println();
+                }
+            });
+        }
+        Thread.sleep(3000);
+        System.out.println("Thread pool size is:"+threadSet.size());
+        System.out.println();
+        fixedThreadPool.execute(() -> {
+            System.out.println("Current thread is from cachedThreadPool："+threadSet.contains(Thread.currentThread().getId()));
+            System.out.println("Current thread id:"+Thread.currentThread().getId());
+            System.out.println("Current thread name:"+Thread.currentThread().getName());
+            System.out.println();
+        });
+    }
 }
