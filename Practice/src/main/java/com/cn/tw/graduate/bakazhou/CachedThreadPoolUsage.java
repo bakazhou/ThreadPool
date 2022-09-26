a77f6dabbc229b5f50037c04eeff730c14aa9e99
bakazhou <706515174@qq.com>
2022-09-26 11:48:28 +0800

    feat: completed CachedThreadPoolUsage example

--- Practice/src/main/java/com/cn/tw/graduate/bakazhou/CachedThreadPoolUsage.java
+++ Practice/src/main/java/com/cn/tw/graduate/bakazhou/CachedThreadPoolUsage.java
@@ -1,4 +1,29 @@
 package com.cn.tw.graduate.bakazhou;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class CachedThreadPoolUsage {
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService cachedThreadPool = Executors.newCachedThreadPool();
+        Object lock = new Object();
+        for (int i = 0; i < 5; i++) {
+            cachedThreadPool.execute(() -> {
+                synchronized (lock){
+                    System.out.println("Current thread id:"+Thread.currentThread().getId());
+                    System.out.println("Current thread name:"+Thread.currentThread().getName());
+                    System.out.println();
+                }
+            });
+        }
+
+        Thread.sleep(5000);
+        cachedThreadPool.execute(() -> {
+            System.out.println("Current thread id:"+Thread.currentThread().getId());
+            System.out.println("Current thread name:"+Thread.currentThread().getName());
+            System.out.println();
+        });
+
+        cachedThreadPool.shutdown();
+    }
 }
